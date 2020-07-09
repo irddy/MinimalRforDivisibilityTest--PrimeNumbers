490a44e36679c3b1ad0ad83a037c1dc86f6d78a8
Irdi Duka <dukairdi@gmail.com>
2020-07-09 20:27:17 +0300

    Add files via upload
    This program will find the smallest integer r that proves that a divisibility test for any prime number up to 999983 (2 and 5 not included.)

---
+++ MinimalRforDevisibilityTest.py
@@ -0,0 +1,34 @@
+prime = int(input("Please insert a prime number: ")) 
+
+integersArray = [1,2,3,4,5,6,7,8,9]
+primeMultiplied = []
+theOnesWithNineOrOne = [] 
+
+
+if prime >=3 & prime <= 999983 & prime != 5: 
+  for element in integersArray: 
+    temp = element * prime
+    primeMultiplied.append(temp)
+  
+  for N in primeMultiplied: 
+    print(N)
+
+
+print()
+print()
+
+
+for elem in range(0,9): 
+  temp1 = primeMultiplied[elem]
+  temp2 = str(temp1)
+
+
+  if temp2[-1] == '9' or temp2[-1] == '1': 
+    theOnesWithNineOrOne.append(temp2)
+
+
+
+theNumberWeNeed = min(theOnesWithNineOrOne)
+
+print(theNumberWeNeed)
+
